81ac126526afc121cfd4dc74056672dad0e5f6e7
ShiJiliana <ulgulakova@yandex.ru>
2024-04-18 13:15:15 +0700

    add

--- 3.py
+++ 3.py
@@ -1,13 +1,13 @@
 n = int(input('Введите кол-во упаковок мороженого: '))
-delitel = []
+dividers = []
 d = 0
 for i in range(1, n + 1):
     if n % i == 0:
-        delitel.append(i)
+        dividers.append(i)
         d += 1
 if n == 2:
     result = n
 else:
-    result = delitel[1]
+    result = dividers[1]
 
 print(result)
