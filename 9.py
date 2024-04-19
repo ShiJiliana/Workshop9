d161dd13a430c690fd5cc609d87b10c64bd92272
ShiJiliana <ulgulakova@yandex.ru>
2024-04-20 03:28:04 +0700

    add

--- 9.py
+++ 9.py
@@ -1,9 +1,9 @@
 n = int(input())
 d = 0
-for x in range(0, n + 1):
-    for y in range(0, n+1):
-        if x == y:
-            d += 2*x
-        if x != y:
-            d += 0.5*(x+y)
+for i in range(0, n + 1):
+    for j in range(0, n+1):
+        if i == j:
+            d += 2*i
+        if i != j:
+            d += 0.5*(i+j)
 print(int(d))
